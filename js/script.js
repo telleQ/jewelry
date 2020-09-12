c74497233801763c404a460b6d43815eaaa460e5
telleQ <STEP.nst@gmail.com>
2020-09-12 21:11:15 +0300

    upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//первая строка нужна, чтобы скрипт начал срабатывать только после загрузки страницы.
+$(document).ready(function () {
+  $('.header__burger').click(function (event) {
+    $('.header__burger,.header__menu').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+  $('.header__menu').click(function () {
+    $('.header__burger, .header__menu').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+
+});
